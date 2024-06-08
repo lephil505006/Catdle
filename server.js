cd8afa8b99c709c7f1c880b697f1fd12b03703be
Philip Le <ple2@hawk.iit.edu>
2024-06-08 00:19:50 -0500

    Adding comment guidelines for future

--- server.js
+++ server.js
@@ -14,3 +14,15 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Catdle game is running on http://localhost:${port}`);
 });
+
+/* 
+Categories
+IMAGES: self explanatory 
+Rarity: Legend Rare, Uber Super Rare, Super Rare, Rare, Special, Normal
+Source: Specific gacha banner, advent stage, legend stage, etc.
+Role: Sniper, LD, Tank... Go on wiki plz
+Target: Which traits they target
+Abilities: What abilities the cat has
+Cost: price of cat, up or down
+Version: When cat was added (only normal form NOT TRUE FORM)
+*/
